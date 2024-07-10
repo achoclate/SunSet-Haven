5be2430e4219711a450859dcd8f37cf4dac4433b
achoclate <nyabokeann99@gmail.com>
2024-07-11 01:47:01 +0300

    create a header component and add the navlinks

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import "./Header.css";
+// import { Link, NavLink } from "react-router-dom";
+// import { BiMenuAltRight } from "react-icons/bi";
+// import { getMenuStyles } from "../../utils/common";
+// import useHeaderColor from "../../hooks/useHeaderColor";
+// import OutsideClickHandler from "react-outside-click-handler";
+
+const Header = () => {
+  const [menuOpened, setMenuOpened] = useState(false);
+  const headerColor = useHeaderColor();
+
+  return (
+    <section className="h-wrapper" style={{ background: headerColor }}>
+      <div className="flexCenter innerWidth paddings h-container">
+        <Link to="/">
+        {/* logo */}
+        <img src="./SHP.PNG" alt="logo" width={60} />
+         </Link> 
+        {/* menu */}
+        <OutsideClickHandler
+          onOutsideClick={() => {
+            setMenuOpened(false);
+          }}
+        >
+          <div
+            // ref={menuRef}
+            className="flexCenter h-menu"
+            style={getMenuStyles(menuOpened)}
+          >
+            <NavLink to="/properties">Properties</NavLink>
+            <NavLink to="/value">Our Value</NavLink>
+
+              <a href="mailto:nyabokeann99@gmail.com">Contact</a>
+           {/* login button */}
+           {/* {!isAuthenticated ? ( */}
+              {/* <button className="button" >
+                Login
+              </button> */}
+            
+          </div>
+        </OutsideClickHandler>
+
+        {/* for medium and small screens */}
+        {/* <div
+          className="menu-icon"
+          onClick={() => setMenuOpened((prev) => !prev)}
+        >
+          <BiMenuAltRight size={30} />
+        </div> */}
+      </div>
+    </section>
+  );
+};
+
+export default Header;
