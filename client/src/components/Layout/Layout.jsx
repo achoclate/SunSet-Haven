47e7b0ad6c37b4743db6be535d48593966e44a7c
achoclate <nyabokeann99@gmail.com>
2024-07-11 01:33:26 +0300

    create a layout for the website

---
+++ client/src/components/Layout/Layout.jsx
@@ -0,0 +1,21 @@
+import React, { useContext, useEffect } from "react";
+// import Header from "../Header/Header";
+// import Footer from "../Footer/Footer";
+// import { Outlet } from "react-router-dom";
+
+const Layout = () => {
+
+
+
+  return (
+    <>
+      <div>
+        <Header />
+        <Outlet />
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
