dd9b80fbecda7eb3416973be725543bacdeaced5
achoclate <nyabokeann99@gmail.com>
2024-07-11 01:35:22 +0300

    import footer fot the layout

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect } from "react";
 // import Header from "../Header/Header";
-// import Footer from "../Footer/Footer";
+import Footer from "../Footer/Footer";
 import { Outlet } from "react-router-dom";
 
 const Layout = () => {
