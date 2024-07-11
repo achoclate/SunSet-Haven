b63568f0a7de914a999ed9c8450bb4aab9566af5
MUSTIADO <farhanmaalim6@gmail.com>
2024-07-11 10:00:22 +0300

    second-commit

--- server/models.py
+++ server/models.py
@@ -1,2 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
 from uuid import uuid4
+
+db = SQLAlchemy()
+
+def get_uuid():
+    return uuid4().hex
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(100), nullable=False)  # Note: In practice, hash passwords before saving
+    
